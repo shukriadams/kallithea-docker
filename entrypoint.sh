f8f2ad4ac995ea0fba38e0114497b05e80ecca47
Shukri Adams <shukri.adams@gmail.com>
2019-09-02 15:36:00 +0200

    moved init flag to repos folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -f /opt/kallithea/data/__isInitialized ]; then
+if [ ! -f /opt/kallithea/repos/__isInitialized ]; then
     echo "Initializing Kallithea"
 
     # this is the setup command for 0.4x
@@ -9,8 +9,8 @@ if [ ! -f /opt/kallithea/data/__isInitialized ]; then
     
     yes "y" | paster setup-db /opt/kallithea/config.ini --user=admin --password=admin --email=admin@admin.com --repos=/opt/kallithea/repos
 
-    # create a file in persistent data folder so we know db has been initialized
-    touch /opt/kallithea/data/__isInitialized
+    # create a file in persistent repositories folder so we know db has been initialized
+    touch /opt/kallithea/repos/__isInitialized
 fi
 
 # start script for 0.4.x
